c734ee28d413ab20d38f026a24f552318160e40b
Raghav893 <raghavarora2309@gmail.com>
2026-06-06 16:57:09 +0530

    init

---
+++ src/main/java/com/raghav/runboxspringboot/RunBoxSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.raghav.runboxspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunBoxSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunBoxSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raghav/runboxspringboot/RunBoxSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghav.runboxspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunBoxSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
