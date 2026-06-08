96e2142a7284d1a590d275048212a8eb6df83a6a
Raghav893 <raghavarora2309@gmail.com>
2026-06-08 14:41:07 +0530

    remove unused imports in SubmissionRepository

--- src/main/java/com/raghav/runboxspringboot/submit/repo/SubmissionRepository.java
+++ src/main/java/com/raghav/runboxspringboot/submit/repo/SubmissionRepository.java
@@ -1,12 +1,12 @@
 package com.raghav.runboxspringboot.submit.repo;
 
-import com.raghav.runboxspringboot.submit.dto.SubmissionResponseDTO;
+
 import com.raghav.runboxspringboot.submit.entity.Submission;
 import com.raghav.runboxspringboot.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.lang.ScopedValue;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
