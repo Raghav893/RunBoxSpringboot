257e036fddba7e5273ec6c2c2eba531f9a3b35e8
Raghav893 <raghavarora2309@gmail.com>
2026-06-14 15:34:51 +0530

    update execeution repo

--- src/main/java/com/raghav/runboxspringboot/execution/repo/ExecutionRepository.java
+++ src/main/java/com/raghav/runboxspringboot/execution/repo/ExecutionRepository.java
@@ -1,9 +1,15 @@
 package com.raghav.runboxspringboot.execution.repo;
 
 import com.raghav.runboxspringboot.execution.entity.Execution;
+import com.raghav.runboxspringboot.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ExecutionRepository extends JpaRepository<Execution, UUID> {
+    Optional<Execution> findBySubmissionSubmissionIdAndSubmissionUser(UUID submissionId, User user);
+
+    List<Execution> findBySubmissionUser(User user);
 }
